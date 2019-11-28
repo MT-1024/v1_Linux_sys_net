77320d571c18652ba7d79b0ce50424bb72c26aee
wjf <944926942@qq.com>
2019-11-28 17:18:01 +0800

    共享库和静态库制作

---
+++ 01_.so_.a/head.h
@@ -0,0 +1,7 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int dive(int a, int b);
+#endif
---
+++ 01_.so_.a/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "head.h"
+
+int main()
+{
+    printf("%d\n", add(5, 5));
+    return 0;
+}
