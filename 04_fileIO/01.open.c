4e5684087f7d0f2e57cf37c167d062c193a7416b
wjf <944926942@qq.com>
2019-11-29 17:11:48 +0800

    linux file open api

--- 04_fileIO/01.open.c
+++ 04_fileIO/01.open.c
@@ -1,14 +1,23 @@
+//file open() write()
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char * argv[])
 {
-    int fd = open(argv[1] , O_CREAT, 0644);
+    char buf[1024] = "helloWorld";
+    printf("buff strlen = %ld\n", strlen(buf));
+    printf("buff sizeof = %ld\n", sizeof(buf));
+    int fd = open(argv[1] , O_CREAT | O_RDWR, 0644);
     printf("fd = %d\n", fd);
+
+    int ret = write(fd, buf, strlen(buf));
+    printf("ret = %d\n", ret);
     close(fd);
+
     return 0;
 }
