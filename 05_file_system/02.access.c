75ecb75c9d786c9c7c6b40d4d8ad97e022b9bd67
wjf <944926942@qq.com>
2019-12-06 09:51:23 +0800

    检查文件是否存在、可读、可写

--- 05_file_system/02.access.c
+++ 05_file_system/02.access.c
@@ -1,9 +1,11 @@
+//检查文件是否存在、可读、可写
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main()
 {
+    //检查文件是否存在
     if(access("./hello", F_OK) == -1)
     {
         perror("access error");
