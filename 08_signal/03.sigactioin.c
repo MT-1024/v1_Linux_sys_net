8f776b557c088c5ff5b1e1bed9f7de8c54a745d6
wjf <944926942@qq.com>
2019-12-20 17:20:38 +0800

    捕捉信号并执行相应动作

---
+++ 08_signal/03.sigactioin.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+
+void catch_sig_action(int num)
+{
+    printf("catch signal : %d\n", num);
+}
+
+int main()
+{
+    struct sigaction act;
+    act.sa_handler = catch_sig_action;
+
+    /* 指定捕捉信号后执行默认动作 */
+    act.sa_handler = SIG_DFL;
+
+    /* 内核在执行某信号对应动作过程中，此时不会处理sa_mask中设置了屏蔽的信号；主要防止信号处理嵌套 */
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+
+    /* 捕捉到SIGINT信号后，执行相应动作 */
+    sigaction(SIGINT, &act, NULL);
+
+    while(1)
+    {
+        /* 模拟一直在运行的进程，在收到SIGINT信号后，进行捕捉，并执行相应的动作 */
+        printf("************\n");
+        sleep(1);
+    }
+
+    return 0;
+}
