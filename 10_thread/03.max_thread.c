05d3455a33529d7dd58987a08d7e27c0b8b8c221
wjf <944926942@qq.com>
2019-12-30 00:47:24 +0800

    测试主机最大能创建线程个数

--- 10_thread/03.max_thread.c
+++ 10_thread/03.max_thread.c
@@ -1,3 +1,4 @@
+/* 测试主机最大创建线程个数 */
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
@@ -18,12 +19,14 @@ int main()
 
     while(1)
     {
+        /* 循环创建子线程 */
         err = pthread_create(&tid, NULL, th_fun, NULL);
         if(err != 0)
         {
             printf("%s\n", strerror(err));
             exit(1);
         }
-        
+        printf("%d\n", i++);
     }
+    return 0;
 }
